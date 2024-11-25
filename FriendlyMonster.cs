0afe52f40ebe5db6d8765b0a9e6f2fcf6b1be93a
sproutedpotato <udyr779@naver.com>
2024-11-25 22:20:17 +0900

    feat: 우호적인 몬스터 추가
    상속을 통해 다양한 몬스터 생성 예정

---
+++ FriendlyMonster.cs
@@ -0,0 +1,11 @@
+﻿class FriendlyMonster : FMonster
+{
+    public void Attack() { }
+    public void Walk(ConsoleKeyInfo key) { }
+    public void Skill() { }
+    public void Defense() { }
+    public void State() { }
+    public void Death() { }
+    public void Create() { }
+}
+
