2f43b94c72540cf67e6d085f59a040a5fa1036a6
sproutedpotato <udyr779@naver.com>
2024-11-25 22:27:18 +0900

    feat: 초기 매니저 추가
    게임의 작동을 검사하기 위한 매니저

---
+++ InitManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+class InitManager
+{
+    private bool isError;
+    private static InitManager init = null;
+
+    private InitManager()
+    {
+        isError = false;
+    }
+    public static InitManager GetInstance()
+    {
+        if (init == null) { init = new InitManager(); }
+        return init;
+    }
+
+    public bool DectetCondition(String tag, bool condition) {
+        if (!condition) { Console.WriteLine("game condition is bad. check your game condtion"); return false; } 
+        else { Console.WriteLine($"{tag} game start..."); return true; }
+    }
+    
+}
