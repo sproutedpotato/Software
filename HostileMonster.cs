77ee35599288ead4adc51070be3f7002d79791bd
sproutedpotato <udyr779@naver.com>
2024-11-25 22:26:09 +0900

    feat: 적대적인 몬스터 추가
    상속을 통해 다양한 몬스터 생성 예정

---
+++ HostileMonster.cs
@@ -0,0 +1,11 @@
+﻿class HostileMonster : FMonster
+{
+    public void Attack() { }
+    public void Walk(ConsoleKeyInfo key) { }
+    public void Skill() { }
+    public void Defense() { }
+    public void State() { }
+    public void Death() { }
+    public void DetectApproch() { }
+    public void Create() { }
+}
