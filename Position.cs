6986cbd5050f5f19f3fec947398ab357f241c6f6
sproutedpotato <udyr779@naver.com>
2024-11-27 23:50:09 +0900

    feat: 위치 정보를 저장하기 위한 클래스
    위치 정보 저장용

---
+++ Position.cs
@@ -0,0 +1,27 @@
+﻿public class Position
+{
+    public int x;
+    public int y;
+    
+    public Position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int GetPosition_X()
+    {
+        return x;
+    }
+
+    public int GetPosition_Y()
+    {
+        return y;
+    }
+
+    public void SetPosition(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
