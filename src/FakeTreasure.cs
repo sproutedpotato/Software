19386ff5136d0db2f8688290e8f6ae3e2b9db8f5
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-13 23:32:56 +0900

    feat : 맵 내의 FakeTreasure 클래스 추가
    발견할 수 있는 가짜 보물, 점수를 깎음.

---
+++ src/FakeTreasure.cs
@@ -0,0 +1,40 @@
+﻿public class FakeTreasure : Treasure
+{
+    private string name;
+    private int score;
+    private bool dropped;
+
+    public FakeTreasure()
+    {
+        score = -100;
+        name = "Fake Treasure";
+        dropped = true;
+    }
+
+    public override string GetItem()
+    {
+        return name;
+    }
+    public override void UseItem(string item)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"You Get {name}!! Your Score Has Decreased!!");
+        Console.ResetColor();
+    }
+    public override string GetItemKind()
+    {
+        return name;
+    }
+    public override int GetScore()
+    {
+        return score;
+    }
+    public override bool GetDropped()
+    {
+        return dropped;
+    }
+    public override void SetDropped(bool value)
+    {
+        dropped = value;
+    }
+}
