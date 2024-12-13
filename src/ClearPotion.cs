60cd8ed7793db312b663ce64cf870214c50edd7e
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-13 23:30:03 +0900

    feat  : ClearPotion 클래스 추가
    유저의 상태를 해제해주는 기능은 GameManager에 구현

---
+++ src/ClearPotion.cs
@@ -0,0 +1,36 @@
+﻿public class ClearPotion : UserItem
+{
+    private string name;
+    private string kind;
+    private bool dropped { get; set; }
+
+    public ClearPotion()
+    {
+        name = "ClearPotion";
+        kind = "UserItem";
+        dropped = true;
+    }
+
+    public override string GetItem()
+    {
+        return name;
+    }
+    public override void UseItem(string item)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"You Use {item}.");
+        Console.ResetColor();
+    }
+    public override string GetItemKind()
+    {
+        return kind;
+    }
+    public override bool GetDropped()
+    {
+        return dropped;
+    }
+    public override void SetDropped(bool value)
+    {
+        dropped = value;
+    }
+}
