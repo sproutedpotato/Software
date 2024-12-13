de82cac8de08ca5e76d9eb56dec29628b4967961
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-13 23:35:52 +0900

    feat : 코드 완성에 따른 새로운 기능 추가
    MonsterKind, GetDropItem 메서드 추가

--- src/FriendlyMonster.cs
+++ src/FriendlyMonster.cs
@@ -1,11 +1,28 @@
-﻿class FriendlyMonster : FMonster
+﻿public class FriendlyMonster : FMonster
 {
-    public void Attack() { }
-    public void Walk(ConsoleKeyInfo key) { }
-    public void Skill() { }
-    public void Defense() { }
-    public void State() { }
-    public void Death() { }
-    public void Create() { }
+
+    public virtual int Attack() {
+        return -1;
+    }
+    public virtual int Skill() 
+    {
+        return -1;
+    }
+    public virtual void Defense(int damage) { }
+    public virtual string State() 
+    {
+        return "";
+    }
+    public virtual bool Death() {
+        return false;
+    }
+    public virtual int GetHp(int num, string s) { return -1; }
+    public virtual int GetScore() {
+        return -1;
+    }
+    public virtual string GetState() { return "";  }
+    public virtual void SetState(string s) { }
+    public virtual string MonsterKind() { return ""; }
+    public virtual string GetDropItem() { return ""; }
 }
 
