32fc59ea51ee01f065b28ce8ed095ea3527641db
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-13 23:29:17 +0900

    feat : 아드로핀 클래스 추가
    유저의 공격력을 증가시켜주는 기능은 GameManager에 구현

---
+++ src/Adropine.cs
@@ -0,0 +1,36 @@
+﻿public class Adropine : UserItem
+{
+    private string name;
+    private string kind;
+    private bool dropped { get; set; }
+
+    public Adropine()
+    {
+        name = "Adropine";
+        kind = "UserItem";
+        dropped = true;
+    }
+
+    public override string GetItem()
+    {
+        return name;
+    }
+    public override void UseItem(string item)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"You Use {item}.");
+        Console.ResetColor();
+    }
+    public override string GetItemKind()
+    {
+        return kind;
+    }
+    public override bool GetDropped()
+    {
+        return dropped;
+    }
+    public override void SetDropped(bool value)
+    {
+        dropped = value;
+    }
+}
