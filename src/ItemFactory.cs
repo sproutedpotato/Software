36533f0d2f708d77c6dbfd6d314f4469c89e2633
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-14 23:53:55 +0900

    feat : 유저 아이템 팩토리 추가
    유저 아이템을 만들어내는 팩토리 패턴

---
+++ src/ItemFactory.cs
@@ -0,0 +1,30 @@
+﻿public class ItemFactory
+{
+    public FUserItem CreateItem(string type)
+    {
+        if (type == "healingpotion")
+        {
+            return new HealingPotion();
+        }
+        else if (type == "manapotion")
+        {
+            return new ManaPotion();
+        }
+        else if (type == "clearpotion")
+        {
+            return new ClearPotion();
+        }
+        else if (type == "hiderobe")
+        {
+            return new HideRobe();
+        }
+        else if (type == "adropine")
+        {
+            return new Adropine();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
