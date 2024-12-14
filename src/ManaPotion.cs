ce55a10087900302877e916f20da06e73f61f6f4
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-14 23:54:34 +0900

    feat : ManaPotion 클래스 추가
    유저의 마나를 회복시켜주는 기능은 GameManager에 구현

---
+++ src/ManaPotion.cs
@@ -0,0 +1,36 @@
+﻿public class ManaPotion : UserItem
+{
+    private string name;
+    private string kind;
+    private bool dropped { get; set; }
+
+    public ManaPotion()
+    {
+        name = "ManaPotion";
+        kind = "UserItem";
+        dropped = true;
+    }
+
+    public override string GetItem()
+    {
+        return name;
+    }
+    public override void UseItem(string item)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"You Use {item}.");
+        Console.ResetColor();
+    }
+    public override string GetItemKind()
+    {
+        return kind;
+    }
+    public override bool GetDropped()
+    {
+        return dropped;
+    }
+    public override void SetDropped(bool value)
+    {
+        dropped = value;
+    }
+}
