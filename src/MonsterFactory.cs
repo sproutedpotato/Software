7df9e9aea3c879019f673a3ae85072e50a09be1f
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-14 23:56:01 +0900

    feat : 몬스터 팩토리 추가
    맵 내의 몬스터를 생성하는 팩토리

---
+++ src/MonsterFactory.cs
@@ -0,0 +1,30 @@
+﻿public class MonsterFactory
+{
+    public FMonster CreateMonster(string type)
+    {
+        if(type == "slime")
+        {
+            return new Slime();
+        }
+        else if(type == "mushroom")
+        {
+            return new Mushroom();
+        }
+        else if(type == "ogre")
+        {
+            return new Ogre();
+        }
+        else if(type == "devil")
+        {
+            return new Devil();
+        }
+        else if(type == "dragon")
+        {
+            return new Dragon();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
