897df7bc669b1250059693fce68f1ebd63a3071a
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-14 23:51:28 +0900

    feat :  HealingPotion 클래스 추가
    유저의 체력을 회복시켜주는 기능은 GameManager에 구현

---
+++ src/HealingPotion.cs
@@ -0,0 +1,36 @@
+﻿public class HealingPotion : UserItem
+{ 
+    private string name;
+    private string kind;
+    private bool dropped { get; set; }
+
+    public HealingPotion()
+    {
+        name = "HealingPotion";
+        kind = "UserItem";
+        dropped = true;
+}
+
+    public override string GetItem()
+    {
+        return name;
+    }
+    public override void UseItem(string item)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"You Use {item}.");
+        Console.ResetColor();
+    }
+    public override string GetItemKind()
+    {
+        return kind;
+    }
+    public override bool GetDropped()
+    {
+        return dropped;
+    }
+    public override void SetDropped(bool value)
+    {
+        dropped = value;
+    }
+}
