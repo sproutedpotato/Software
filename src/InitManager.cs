14d0902a1b56b0112a358e27f285b733ae5865b3
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-14 23:53:20 +0900

    feat : InitManager 추가
    singleton pattern - 테스트용도의 코드. game start의 역할

--- src/InitManager.cs
+++ src/InitManager.cs
@@ -1,21 +1,15 @@
-﻿using System.Runtime.InteropServices;
-
-class InitManager
+﻿class InitManager
 {
-    private bool isError;
     private static InitManager init = null;
 
-    private InitManager()
-    {
-        isError = false;
-    }
+    private InitManager() { }
     public static InitManager GetInstance()
     {
         if (init == null) { init = new InitManager(); }
         return init;
     }
 
-    public bool DectetCondition(String tag, bool condition) {
+    public bool DectetCondition(string tag, bool condition) {
         if (!condition) { Console.WriteLine("game condition is bad. check your game condtion"); return false; } 
         else { Console.WriteLine($"{tag} game start..."); return true; }
     }
