ed292904596d63710a15f2dd6f8c63cdd3b44134
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-16 09:02:47 +0900

    feat : Treasure 클래스 추가
    TrueTreasure, FakeTreasure의 부모 클래스인 Treasure 클래스 추가

---
+++ src/Treasure.cs
@@ -0,0 +1,26 @@
+﻿public class Treasure : FTreasure
+{
+    private int score;
+    private string name;
+    private bool dropped;
+
+    public virtual string GetItem()
+    {
+        return name;
+    }
+    public virtual void UseItem(string item) { }
+
+    public virtual string GetItemKind()
+    {
+        return name;
+    }
+    public virtual int GetScore()
+    {
+        return score;
+    }
+    public virtual bool GetDropped()
+    {
+        return dropped;
+    }
+    public virtual void SetDropped(bool dropped) { }
+}
