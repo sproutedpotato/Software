a0d33bee4463482547253cf6c011f3d63f62d89e
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-16 09:05:42 +0900

    feat : UserFactory 클래스 추가
    본 게임에서는 하나의 유저만 있지만, 재사용 시 멀티 플레이어, 다양한 유저 몬스터를 생성할 수 있는 UserFactory 추가

---
+++ UserFactory.cs
@@ -0,0 +1,14 @@
+﻿public class UserFactory
+{
+    public User CreateUser(string type)
+    {
+        if (type == "Player")
+        {
+            return new Player();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
