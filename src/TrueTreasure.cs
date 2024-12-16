dd4e3cd41d7d82e0c850ecaf9d2b681204a7a1d0
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-16 09:03:59 +0900

    feat : TrueTreasure 클래스 추가
    진짜 보물을 담당하는 TrueTreasure 클래스 추가

---
+++ src/TrueTreasure.cs
@@ -0,0 +1,42 @@
+﻿public class TrueTreasure : Treasure
+{
+    private int score;
+    private string name;
+    private string kind;
+    private bool dropped;
+
+    public TrueTreasure()
+    {
+        name = "True Treasure";
+        kind = "Treasure";
+        score = 1000;
+        dropped = true;
+    }
+
+    public override string GetItem()
+    {
+        return name;
+    }
+    public override void UseItem(string item)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"You Get {name}!! Score + 1000");
+        Console.ResetColor();
+    }
+    public override string GetItemKind()
+    {
+        return kind;
+    }
+    public override int GetScore()
+    {
+        return score;
+    }
+    public override bool GetDropped()
+    {
+        return dropped;
+    }
+    public override void SetDropped(bool value)
+    {
+        dropped = value;
+    }
+}
