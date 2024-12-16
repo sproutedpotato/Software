0fa20b9217a1270beff7e48b48474abc0e701e73
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-16 09:03:21 +0900

    feat : TreasureFactory 추가
    보물들의 생성을 담당하는 TreasureFactory 추가

---
+++ src/TreasureFactory.cs
@@ -0,0 +1,18 @@
+﻿public class TreasureFactory
+{
+    public FTreasure CreateTreasure(string type)
+    {
+        if (type == "TrueTreasure")
+        {
+            return new TrueTreasure();
+        }
+        else if (type == "FakeTreasure")
+        {
+            return new FakeTreasure();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
