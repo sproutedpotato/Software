c6717fbdebd639d9a702cf52c5aaab44fb300902
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-16 09:01:33 +0900

    feat : 1단계 몬스터 Slime 추가
    코드 재사용을 통해 만들어진 Slime 몬스터

--- src/Slime.cs
+++ src/Slime.cs
@@ -1,45 +1,115 @@
-﻿class Slime : FriendlyMonster
+﻿public class Slime : FriendlyMonster
 {
     private int hp { get; set; }
     private int mp { get; set; }
+    private int damage { get; set; }
+    private int score { get; set; }
     private string state { get; set; }
-    private Position pos { get; set; }
-    private Random rand = new Random();
+    private string dropitem { get; set; }
+    private string[] states = { "Freeze" };
+    private Random rand;
+    private bool operateState { get; set;}
+    private bool isDead;
 
-    private Slime() { }
-
-    protected int getSlime(string what)
+    public Slime()
     {
-        if (what == "mp") { return mp; }
-        else if (what == "hp") { return hp; }
-        else { return -1; }
-
+        hp = 100;
+        mp = 10;
+        state = "Normal";
+        damage = 10;
+        rand = new Random();
+        isDead = false;
+        score = 1000;
+        operateState = false;
+        dropitem = "Jelly";
     }
-    public void Attack() {
+
+    public override int Attack() 
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Silme Attacks Player!");
+        Console.ResetColor();
+        if (mp < 10)
+        {
+            mp += 1;
+        }
+        return damage;
     }
-    public void Walk(ConsoleKeyInfo key)
-    {
-        if (key.Key == ConsoleKey.UpArrow) { pos.SetPosition(pos.x, pos.y + 1); }
-        else if (key.Key == ConsoleKey.DownArrow) { pos.SetPosition(pos.x, pos.y - 1); }
-        else if (key.Key == ConsoleKey.RightArrow) { pos.SetPosition(pos.x + 1, pos.y); }
-        else { pos.SetPosition(pos.x - 1, pos.y); }
-    }
-    public void Skill()
+
+    public override int Skill()
     {
         string[] skills = { "공격1", "공격2", "공격3" };
+        int[] damages = { 15, 20, 25 };
         int[] use_mp = { 2, 3, 5 };
 
         int skill_index = rand.Next(0, skills.Length);
 
-        if (use_mp[skill_index] < mp) { Attack(); mp += 1; }
+        if (use_mp[skill_index] > mp) { Attack(); }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"Slime Uses {skills[skill_index]} to Player! ");
+            Console.ResetColor();
             mp -= use_mp[skill_index];
+
+            int setState = rand.Next(0, 5);
+            if (setState == 1) { operateState = true; }
+        }
+
+        return damages[skill_index];
+    }
+    public override void Defense(int damage) 
+    {
+        hp -= damage;
+        if (hp <= 0) isDead = true;
+    }
+    public override string State()
+    {
+        if (operateState)
+        {
+            string returnValue = states[rand.Next(0, states.Length)];
+            operateState = false;
+            return returnValue;
+        }
+        else
+        {
+            return "Normal";
         }
     }
-    public void Defense() { }
-    public void State() { }
-    public void Death() { }
+    public override string GetState()
+    {
+        return state;
+    }
+    public override void SetState(string state)
+    {
+        this.state = state;
+        if (!state.Equals("Normal"))
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Slime's State Changed!! : {state}");
+            Console.ResetColor( );
+        }
+
+    }
+    public override bool Death() {
+        return isDead;
+    }
+    public override int GetHp(int num = 0, string s = "")
+    {
+        if (s == "-") hp -= num;
+        else if (s == "+") hp += num;
+        return hp;
+    }
+    public override int GetScore()
+    {
+        return score;
+    }
+    public override string MonsterKind() 
+    { 
+        return "Friendly"; 
+    }
+    public override string GetDropItem()
+    {
+        return dropitem;
+    }
 }
