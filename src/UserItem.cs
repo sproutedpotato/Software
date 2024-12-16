e169d7f4941fddf5c775166bb872943ff679871e
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-12-16 09:07:05 +0900

    feat : UserItem 클래스 추가
    유저의 아이템들의 부모 클래스 추가

---
+++ src/UserItem.cs
@@ -0,0 +1,24 @@
+﻿public class UserItem : FUserItem
+{
+    private bool dropped;
+    private string name;
+    private string kind;
+
+    public virtual string GetItem()
+    {
+        return name;
+    }
+    public virtual void UseItem(string item)
+    {
+        Console.WriteLine($"You Use {item}.");
+    }
+    public virtual string GetItemKind() 
+    {
+        return kind;
+    }
+    public virtual bool GetDropped() 
+    { 
+        return dropped; 
+    }
+    public virtual void SetDropped(bool value) { }
+}
